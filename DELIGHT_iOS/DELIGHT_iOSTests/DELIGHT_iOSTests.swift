48618a186982328d452a5866aaa3f7394ff83108
yu kyung <ljj4904@gmail.com>
2024-12-30 13:41:48 +0900

    Add .gitignore

---
+++ DELIGHT_iOS/DELIGHT_iOSTests/DELIGHT_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  DELIGHT_iOSTests.swift
+//  DELIGHT_iOSTests
+//
+//  Created by wjdyukyung on 12/30/24.
+//
+
+import XCTest
+@testable import DELIGHT_iOS
+
+final class DELIGHT_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
